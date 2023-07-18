b31ae91ac9951bc070a326397ba0907fed0bc111
Misha Kukarkin <wardenclock@gmail.com>
2023-07-18 22:42:08 +0500

    fixed routes

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  PORT: process.env.PORT || 3000,
-  MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/mestodb',
-  JWT_SECRET: process.env.JWT_SECRET || 'super-strong-secret',
+  PORT: isProduction ? process.env.PORT : process.env.PORT || 3000,
+  MONGO_URL: isProduction ? process.env.MONGO_URL : process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/mestodb',
+  JWT_SECRET: isProduction ? process.env.JWT_SECRET : process.env.JWT_SECRET || 'super-strong-secret',
 };
