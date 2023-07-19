fb445c73087a7ba59a6e33da9ed9749f6735efe6
Misha Kukarkin <wardenclock@gmail.com>
2023-07-19 15:13:40 +0500

    feat: add cookie to frontend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -19,7 +19,7 @@ module.exports.login = (req, res, next) => {
       const secret = JWT_SECRET;
       const token = jwt.sign({ _id: user._id }, secret, { expiresIn: '7d' });
       res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
+        maxAge: 3600000 * 24 * 10,
         httpOnly: false,
         sameSite: true,
       });
