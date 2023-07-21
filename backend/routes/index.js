0aa2280e49ed4ac73ce6d85e7bbd04bb6c67c5a6
Misha Kukarkin <wardenclock@gmail.com>
2023-07-21 10:24:15 +0500

    removed crash endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -30,12 +30,6 @@ router.post('/signup', celebrate({
   }),
 }), createUser);
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 router.use(auth);
 
 router.get('/logout', logout);
